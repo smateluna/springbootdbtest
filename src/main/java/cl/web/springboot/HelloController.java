d7c0d093c744e81f0f604b19cc96040e5cdeaf84
root <root@p2-print01.localdomain>
2024-06-13 14:18:34 -0400

    Intento 1 de mostrar status connection DB

--- src/main/java/cl/web/springboot/HelloController.java
+++ src/main/java/cl/web/springboot/HelloController.java
@@ -2,13 +2,52 @@ package cl.web.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Saludos desde Spring Boot!";
+				Connection conn = null;
+        		ResultSet rs = null;
+        		PreparedStatement stmt = null;
+
+        		try {
+        			Class.net.sourceforge.jtds.jdbc.Driver.newInstance();	//net.sourceforge.jtds.jdbc.Driver
+        		} catch (ClassNotFoundException ex){
+        			throw new ClassNotFoundException("BAD DRIVER");
+        		} catch (InstantiationException e) {
+        			throw new IllegalAccessException("BAD DRIVER");
+        		} catch (IllegalAccessException e) {
+        			throw new IllegalAccessException("BAD DRIVER");
+        		}
+
+        		try {
+        			conn = DriverManager.getConnection("jdbc:jtds:sqlserver://maipo.cbrs.local:1433/flujo", "sa", "sa");	// (jdbc:jtds:sqlserver://maipo.cbrs.local:1433/flujo , sa , sa )
+
+        			String sql = "SELECT DB_NAME() AS DataBaseName";
+
+        			stmt = conn.prepareStatement(sql);
+        			rs = stmt.executeQuery();
+
+        			if(rs.next()){
+        				return "BD OK";
+        			}
+
+        		}catch (SQLException e){
+        			throw new SQLException(e.getMessage());
+        		}finally {
+        			try { if (rs != null) rs.close(); } catch (Exception e) {throw new SQLException(e.getMessage());}
+        			try { if (stmt != null) stmt.close(); } catch (Exception e) {throw new SQLException(e.getMessage());}
+        			try { if (conn != null) conn.close(); } catch (Exception e) {throw new SQLException(e.getMessage());}
+        		}
+        		return "BD NOT OK";
 	}
 
 }
